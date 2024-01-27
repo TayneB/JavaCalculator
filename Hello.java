8dc8e1b3cfdb5f684cec857b91e5b2ffa971dfc7
Tayne Bilyard <taynebilyard@gmail.com>
2024-01-27 19:20:28 +1300

    Java Primitives

--- Hello.java
+++ Hello.java
@@ -1,4 +1,10 @@
-class Hello { public static void main(String a[]) {
-  System.out.println("Hello World");
-}
+class Hello 
+{ 
+  public static void main(String a[]) 
+    {
+      int num1 = 3;
+      int num2 = 33;
+      int result  = num1 + num2;
+      System.out.println(result);
+    }
 }
