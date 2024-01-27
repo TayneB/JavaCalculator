584463035b1693b893324702ce50781d42e4eca0
Tayne Bilyard <taynebilyard@gmail.com>
2024-01-27 19:53:11 +1300

    types of variables

--- Hello.java
+++ Hello.java
@@ -3,8 +3,17 @@ class Hello
   public static void main(String a[]) 
     {
       int num1 = 3;
-      int num2 = 33;
-      int result  = num1 + num2;
+      byte num2 = 127;
+      short sh = 557;
+      long l = 578394;
+
+      float result  = num1 + num2;
+      double b = 34.5;
+
+      char g = 'g';
+
+      boolean t = true;
+
       System.out.println(result);
     }
 }
