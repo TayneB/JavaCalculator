07574c2bef9e60f09162ed4fc08b8be66831e5df
Tayne Bilyard <taynebilyard@gmail.com>
2024-01-29 22:33:50 +1300

    arrow switch statements

--- Hello.java
+++ Hello.java
@@ -1,32 +1,14 @@
 public class Hello {
   public static void main(String a[]) {
-      int n = 1;
+      String day = "Monday";
+      String result = "7am";
 
-      switch (n) {
-          case 1:
-              System.out.println("Monday");
-              break;
-          case 2:
-              System.out.println("Tuesday");
-              break;
-          case 3:
-              System.out.println("Wednesday");
-              break;
-          case 4:
-              System.out.println("Thursday");
-              break;
-          case 5:
-              System.out.println("Friday");
-              break;
-          case 6:
-              System.out.println("Saturday");
-              break;
-          case 7:
-              System.out.println("Sunday");
-              break;
-          default:  
-              System.out.println("Enter a valid day number");
-              break;
-      }
+      result = switch (day) {
+          case "Saturday", "Sunday" -> "6am";
+          case "Monday" ->  "8am";
+          default -> "7am";
+      };
+
+      System.out.println(result);
   }
 }
