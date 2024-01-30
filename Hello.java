329f15e402ddbfddfa8fe48f3cdda6108fbba651
Tayne Bilyard <taynebilyard@gmail.com>
2024-01-30 15:47:11 +1300

    nested for loop

--- Hello.java
+++ Hello.java
@@ -1,9 +1,14 @@
 public class Hello {
   public static void main(String a[]) {
       
-    for(int i = 0; i <= 4; i++)
+    for(int i = 1; i <= 7; i++)
     {
-      System.out.println("Hi " + i);
+      System.out.println("DAY " + i);
+
+      for(int j = 1; j <= 9; j++)
+      {
+        System.out.println("  " + (j + 8) + " - " + (j+9));
+      }
     };
   }
 }
