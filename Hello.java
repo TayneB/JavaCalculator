cff311d203bab0a15028970030d13981c9f67b3f
Tayne Bilyard <taynebilyard@gmail.com>
2024-02-05 15:49:23 +1300

    Stack and Heap

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Calculator
 {
+  int num = 20;
   public int add(int n1, int n2) {
     return n1 + n2;
   }
@@ -7,8 +8,12 @@ class Calculator
 
 public class Hello {
   public static void main(String a[]) {
+      int data = 10;
+
+      Calculator obj1 = new Calculator();
       Calculator calc = new Calculator();
       int result = calc.add(10, 20);
       System.out.println(result);
+      System.out.println(obj1.num);
   }
 }
