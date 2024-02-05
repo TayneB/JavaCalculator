d6ad87740715898162b1e10a917831d7a5940bd5
Tayne Bilyard <taynebilyard@gmail.com>
2024-02-05 13:31:55 +1300

    basic add

--- Hello.java
+++ Hello.java
@@ -1,24 +1,14 @@
-class Computer
+class Calculator
 {
-  public void playMusic() {
-    System.out.println("Playing music");
-  }
-
-  public String getMeAPen(int cost) {
-    if(cost > 10) {
-    return "Pen";
-  } else {
-    return "No Pen";
-  }
-  
+  public int add(int n1, int n2) {
+    return n1 + n2;
   }
 }
 
 public class Hello {
   public static void main(String a[]) {
-      Computer obj = new Computer();
-      obj.playMusic();
-      String str = obj.getMeAPen(10);
-      System.out.println(str);
+      Calculator calc = new Calculator();
+      int result = calc.add(10, 20);
+      System.out.println(result);
   }
 }
