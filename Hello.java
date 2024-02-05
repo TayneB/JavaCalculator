3ff80e605acb42084db6ca3da04a6a09502fe91f
Tayne Bilyard <taynebilyard@gmail.com>
2024-02-05 16:56:50 +1300

    Matrix

--- Hello.java
+++ Hello.java
@@ -1,14 +1,12 @@
 
 public class Hello {
   public static void main(String a[]) {
-      int arrLength = 5;
-      int nums[] = new int[arrLength];
-      nums[0] = 3;
-      nums[1] = 9;
-      nums[2] = 7;
-      nums[3] = 5;
-      for(int i=0; i<arrLength; i++){
-      System.out.println(nums[i]);
+     int num[][] = new int[4][5];
+
+     for(int i=0;i<4;i++) {
+      for(int j=0;j<5;j++) {
+        System.out.print(num[i][j] + " ");
       }
-  }
-}
+      System.out.println();
+     }
+}}
